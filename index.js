f04923eb8e94d21b4fba6e784b8cb0d6331f525b
Usama Ehsan <124584350+ehsanusama@users.noreply.github.com>
2023-08-11 14:10:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-(async()=>{
+(async () => {
     // default imports
     const events = require('events');
-    const { exec } = require("child_process")
+    const {
+        exec
+    } = require("child_process")
     const logs = require("discord-logs")
     const Discord = require("discord.js")
-    const { 
-        MessageEmbed, 
-        MessageButton, 
-        MessageActionRow, 
-        Intents, 
-        Permissions, 
-        MessageSelectMenu 
-    }= require("discord.js")
+    const {
+        MessageEmbed,
+        MessageButton,
+        MessageActionRow,
+        Intents,
+        Permissions,
+        MessageSelectMenu
+    } = require("discord.js")
     const fs = require('fs');
     let process = require('process');
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
     // block imports
+    const ms = require("ms")
     let https = require("https")
-    
+
     // define s4d components (pretty sure 90% of these arnt even used/required)
     let s4d = {
         Discord,
-        fire:null,
-        joiningMember:null,
-        reply:null,
-        player:null,
-        manager:null,
-        Inviter:null,
-        message:null,
-        notifer:null,
+        fire: null,
+        joiningMember: null,
+        reply: null,
+        player: null,
+        manager: null,
+        Inviter: null,
+        message: null,
+        notifer: null,
         checkMessageExists() {
             if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
             if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
@@ -38,20 +41,20 @@
 
     // check if d.js is v13
     if (!require('./package.json').dependencies['discord.js'].startsWith("^13.")) {
-      let file = JSON.parse(fs.readFileSync('package.json'))
-      file.dependencies['discord.js'] = '^13.15.1'
-      fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
-      exec('npm i')
-      throw new Error("Seems you arent using v13 please re-run or run `npm i discord.js@13.12.0`");
+        let file = JSON.parse(fs.readFileSync('package.json'))
+        file.dependencies['discord.js'] = '^13.15.1'
+        fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
+        exec('npm i')
+        throw new Error("Seems you arent using v13 please re-run or run `npm i discord.js@13.12.0`");
     }
 
     // check if discord-logs is v2
     if (!require('./package.json').dependencies['discord-logs'].startsWith("^2.")) {
-      let file = JSON.parse(fs.readFileSync('package.json'))
-      file.dependencies['discord-logs'] = '^2.0.0'
-      fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
-      exec('npm i')
-      throw new Error("discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run");
+        let file = JSON.parse(fs.readFileSync('package.json'))
+        file.dependencies['discord-logs'] = '^2.0.0'
+        fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
+        exec('npm i')
+        throw new Error("discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run");
     }
 
     // create a new discord client
@@ -60,7 +63,7 @@
             Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)
         ],
         partials: [
-            "REACTION", 
+            "REACTION",
             "CHANNEL"
         ]
     });
@@ -71,7 +74,7 @@
     })
 
     // upon error print "Error!" and the error
-    process.on('uncaughtException', function (err) {
+    process.on('uncaughtException', function(err) {
         console.log('Error!');
         console.log(err);
     });
@@ -80,25 +83,38 @@
     logs(s4d.client);
 
     // pre blockly code
-    
+
 
     // blockly code
-    await s4d.client.login('MTEzNzc2NzU4MDg1NDE0MDk5OQ.GdZFDR.IpFKUeGOEJ5rIhA51UqkmurZAEnSTPKHTfKZVY').catch((e) => {
-            const tokenInvalid = true;
-            const tokenError = e;
-            if (e.toString().toLowerCase().includes("token")) {
-                throw new Error("An invalid bot token was provided!")
-            } else {
-                throw new Error("Privileged Gateway Intents are not enabled! Please go to https://discord.com/developers and turn on all of them.")
-            }
-        });
-    
-    s4d.client.on('messageCreate', async (s4dmessage) => {
-      if ((s4dmessage.content) == 'hii') {
-        s4dmessage.channel.send({content:String('Walead chawlan nai mro')});
-      }
-    
+    await s4d.client.login('MTEzNjc1ODAyOTcxOTY5OTU3Ng.GuLkTu.CtJfAMnyalLzbOL2n24irT5wNOPOJzh0PpAkdc').catch((e) => {
+        const tokenInvalid = true;
+        const tokenError = e;
+        if (e.toString().toLowerCase().includes("token")) {
+            throw new Error("An invalid bot token was provided!")
+        } else {
+            throw new Error("Privileged Gateway Intents are not enabled! Please go to https://discord.com/developers and turn on all of them.")
+        }
     });
-    
+
+    s4d.client.on('messageCreate', async (s4dmessage) => {
+        if ((/\d/.test((s4dmessage.content)))) {
+            https.get(('https://lms.cgit.pk/api.php?action=discord_action&data_string=' + String(s4dmessage.content)), async resp => {
+                    let data2 = "";
+                    resp.on("data", async chunk => {
+                        data2 += chunk;
+                    });
+                    resp.on("end", async () => {
+                        let data = JSON.parse(data2)
+                        s4dmessage.channel.send((data.msg));
+
+                    });
+                })
+                .on("error", async err => {
+                    console.log("Error: " + err.message);
+                });
+        }
+
+    });
+
     return s4d
 })();
